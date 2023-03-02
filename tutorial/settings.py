d46e5b462b6d923b6d0668c8b7f2f752423e971d
olegonyxov <olegonyxov@gmail.com>
2023-03-02 21:57:56 +0200

    skey

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c=lum_72flj$qe8a22&l8+n70htrc(#&-6tvpwv(&vum2#%bo)'
+SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
